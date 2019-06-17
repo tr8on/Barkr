11d9c2e823a10847ca02701bfc01c1ce9d733db4
Ruben Velez <rubenalexisvelez@gmail.com>
2019-06-17 17:44:11 -0400

    started CreateDogProfile

--- barkr/frontend/Components/CreateDogProfile/index.js
+++ barkr/frontend/Components/CreateDogProfile/index.js
@@ -5,16 +5,80 @@ import {Redirect} from "react-router-dom"
 class  CreateDogProfile extends Component  {
   constructor(){
     super(); 
-  }
+    this.state = {
+        name: "",
+        breed: "",
+        gender: "",
+        zipcode: "",
+        age: "",
+        description: "",
+        redirect: false
+    }
 
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
 
-  render(){
-    return (
-      <div> 
-      </div>
-    );
   }
-}
+    
+      handleChange(event){
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+          [name]: value
+        })    
+      }
+    
+      async handleSubmit(event){
+        event.preventDefault();
+        await axios.post("/Dogs/", {
+          name: this.state.name,
+          description: this.state.description,
+          breed: this.state.breed,
+          gender: this.state.gender,
+          zipcode: this.state.zipcode,
+          age: this.state.age
+        })
+        this.setState({
+          redirect: true
+        })
+      }
+    
+      render(){
+        return (
+          <div> 
+            {this.state.redirect ? <Redirect to="/"/>: null}
+            <form 
+            onChange={this.handleChange} 
+            onSubmit={this.handleSubmit}
+            >
+              <input
+                name="name"
+                type="text"
+                placeholder="name of dog"
+                value={this.state.name}
+              />
+              <input
+                name="description"
+                type="textarea"
+                placeholder="give a description"
+                value={this.state.description}
+                />
+                <input
+                  name="rating"
+                  type="number"
+                  placeholder="give it a rating"
+                  value={this.state.rating}
+                />
+                <br />
+                <input type="submit"
+                />
+    
+              
+            </form>
+          </div>
+        );
+      }
+    }
 
 
 export default CreateDogProfile;
