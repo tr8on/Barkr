03396bcbed65c550e4410d4ecf41c4b8dc816915
Ruben Velez <rubenalexisvelez@gmail.com>
2019-06-20 17:49:47 -0400

    made female list clickable

--- barkr/Client/src/Components/MaleList/index.js
+++ barkr/Client/src/Components/MaleList/index.js
@@ -3,8 +3,7 @@ import axios from "axios";
 import {Link} from "react-router-dom"
 import "./dogList.css"
 import Box from "../DogBox/index"
-import ShowDogList from '../ShowDogList';
-import ShowDogProfile from '../showDogProfile';
+
 
 class  MaleList extends Component  {
 
@@ -24,37 +23,26 @@ class  MaleList extends Component  {
       dogsToShow: dogsToShow
     })
 
-  //   const apiRes = await axios.get(`https://dog.ceo/api/breed/${this.state.dogsToShow.dog.breed.toLowerCase()}/images/random`)
-  // const dogPic = apiRes.data.message
-  // console.log(dogPic)
-  // this.setState({
-  //   img: dogPic
-  // })
-  // console.log(apiRes)
-
   }
 
-
   render(){
-
-   
 
     return (
       <div className="container">
         <br></br>
-        <br></br><br></br>
+        <br></br>
+        <br></br>
        <div className="register"><Link to='dog/create'> Register A Dog </Link></div>
-      <div className="row">
+       <div className="row">
           {this.state.dogsToShow.map ( (dog) => (
-            <Link style={{ textDecoration: 'none' }} key={dog.id} to={`/dog/${dog.id}`}>
-              <Box name={dog.name} id={dog.id} breed={dog.breed} age={dog.age}></Box>
-            </Link>
+          <Link style={{ textDecoration: 'none' }} key={dog.id} to={`/dog/${dog.id}`}>
+            <Box name={dog.name} id={dog.id} breed={dog.breed} age={dog.age}></Box>
+          </Link>
           ))}
+       </div>
       </div>
-</div>
     );
   }
 } 
-
 
 export default MaleList;
